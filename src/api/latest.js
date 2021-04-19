27c3fae8b0419a7492d77f690cd9b5e9168d1d8c
Mehdi ESSALEHI <messalehi2001@outlook.com>
2021-04-19 04:31:22 +0000

    More testing

--- src/api/latest.js
+++ src/api/latest.js
@@ -1,6 +1,24 @@
 exports.handler = async (event, context) => { 
-    return {
-        statusCode: 200,
-        body: "Hello !"
+    try {
+        var request = await fetch("https://cors.bridged.cc/https://anslayer.com/anime/public/animes/get-published-animes?json=" + JSON.stringify({
+                _limit: 30,
+                _order_by: "latest_first",
+                just_info: "Yes"
+            }), { 
+            headers: new Headers({
+                "Client-Id": process.env.REACT_APP_CLIENT_ID,
+                "Client-Secret": process.env.REACT_APP_CLIENT_SECRET,
+            })
+        })
+        var data = await request.json()
+        return {
+            status: 200,
+            body: data
+        }
+    } catch (error) {
+        return {
+            status: 500,
+            body: error
+        }
     }
 }
