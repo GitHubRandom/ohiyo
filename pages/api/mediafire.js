d71c4ba22cd91bb87278dc55be325c02b4e0beea
Mehdi ESSALEHI <messalehi2001@outlook.com>
2021-04-29 23:34:37 +0000

    Another update

--- pages/api/mediafire.js
+++ pages/api/mediafire.js
@@ -1,11 +1,12 @@
 export default async function handler(req, res) {
+    context.log(req.headers)
     const resource = await fetch(req.query.link, {
         headers: new Headers({
-            'Referer': "https://ohiyo-git-theftify-githubrandom.vercel.app/"
+            'User-Agent': req.headers["user-agent"]
         }),
         referrer: "https://ohiyo-git-theftify-githubrandom.vercel.app/"
     })
-    console.log(resource)
+    console.log(resource.headers)
     const data = await resource.text()
     res.status(200).send(data)
 }
