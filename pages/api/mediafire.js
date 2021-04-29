a4617429d9be0a2ba12153c9029a90b23b2a76ea
Mehdi ESSALEHI <messalehi2001@outlook.com>
2021-04-29 23:05:17 +0000

    Testing fixes for mediafire

--- pages/api/mediafire.js
+++ pages/api/mediafire.js
@@ -1,5 +1,11 @@
 export default async function handler(req, res) {
-    const resource = await fetch(req.query.link)
+    const resource = await fetch(req.query.link, {
+        headers: new Headers({
+            'Referer': "https://ohiyo-9ysesuawh-githubrandom.vercel.app/"
+        }),
+        referrer: "https://ohiyo-9ysesuawh-githubrandom.vercel.app/"
+    })
+    console.log(resource)
     const data = await resource.text()
     res.status(200).send(data)
 }
