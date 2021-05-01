64aefbebd24fb608444b73f1b9a5e470f0c95e40
Mehdi ESSALEHI <messalehi2001@outlook.com>
2021-05-01 18:27:23 +0000

    Another test

--- pages/api/mediafire.js
+++ pages/api/mediafire.js
@@ -1,18 +1,25 @@
-var http = require('http')
+var http = require('follow-redirects').https
+var requestIp = require('request-ip')
 
 export default async function handler(req, res) {
-    let data = ""
     let finished = false
+    var url = new URL(req.query.link)
 
     const options = {
-        host: req.query.link,
-        localAddress: req.connection.remoteAddress
+        method: "GET",
+        host: url.hostname,
+        path: url.pathname + `${url.search ? url.search : ""}`,
+        headers: {
+            'X-Real-IP': requestIp.getClientIp(req),
+            'X-Forwarded-For': requestIp.getClientIp(req)
+        }
     }
     console.log(options)
     http.request(options, response => {
+        let data = ""
         response.on("data", chunk => {
-            data += chunk
-            console.log(chunk)
+            data += chunk.toString()
+            console.log(chunk.toString())
         })
         response.on("end", () => {
             res.status(200).send(data)
