046928d9f8bd4161d7401e3d18d401d930d90d4b
Mehdi ESSALEHI <messalehi2001@outlook.com>
2021-05-01 16:31:36 +0000

    Attempt with x-forwarded-for

--- pages/api/mediafire.js
+++ pages/api/mediafire.js
@@ -1,5 +1,9 @@
 export default async function handler(req, res) {
-    const resource = await fetch(req.query.link)
+    const resource = await fetch(req.query.link, {
+        headers: new Headers({
+            "X-Forwarded-For": req.headers["x-real-ip"]
+        })
+    })
     const data = await resource.text()
     res.status(200).send(data)
 }
