92e6a1ffa4f37ae0a48703e13f6de28286679c59
Mehdi ESSALEHI <messalehi2001@outlook.com>
2021-05-01 18:35:22 +0000

    Another one

--- pages/api/mediafire.js
+++ pages/api/mediafire.js
@@ -5,27 +5,27 @@ export default async function handler(req, res) {
     let finished = false
     var url = new URL(req.query.link)
 
+    console.log(requestIp.getClientIp(req))
+
     const options = {
         method: "GET",
         host: url.hostname,
         path: url.pathname + `${url.search ? url.search : ""}`,
         headers: {
             'X-Real-IP': requestIp.getClientIp(req),
-            'X-Forwarded-For': requestIp.getClientIp(req)
+            'X-Forwarded-For': requestIp.getClientIp(req),
+            'True-Client-IP' : requestIp.getClientIp(req)
         }
     }
-    console.log(options)
     http.request(options, response => {
         let data = ""
         response.on("data", chunk => {
             data += chunk.toString()
-            console.log(chunk.toString())
         })
         response.on("end", () => {
             res.status(200).send(data)
         })
         response.on('error', error => {
-            console.log(error)
             finished = true
         })
     }).end()
