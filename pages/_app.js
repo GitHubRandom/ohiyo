777be24ed5848c0cfb59175c83986333899b5710
Mehdi Essalehi <messalehi2001@outlook.com>
2021-05-12 13:14:21 +0000

    Fixed PopCash adcode

--- pages/_app.js
+++ pages/_app.js
@@ -40,7 +40,7 @@ function MyApp({ Component, pageProps }) {
         var pop_tag = document.createElement('script');pop_tag.src='//cdn.popcash.net/show.js';document.body.appendChild(pop_tag);
         pop_tag.onerror = function() {pop_tag = document.createElement('script');pop_tag.src='//cdn2.popcash.net/show.js';document.body.appendChild(pop_tag)};
         `
-        document.head.appendChild(popAds)
+        document.body.appendChild(popAds)
     },[])
 
     return (
