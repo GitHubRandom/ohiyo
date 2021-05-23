9254cf3f9f4a2b7e731d7ce3e8c6ee4b7e012780
Mehdi Essalehi <messalehi2001@outlook.com>
2021-05-23 01:46:57 +0100

    Reverting tentative fix

--- components/WatchTopBar.tsx
+++ components/WatchTopBar.tsx
@@ -48,7 +48,6 @@ const WatchTopBar = ({ mal, animeId, episodesList, episodeTitle, episodeNumber, 
 
     const [ ascending, updateEpisodesOrder ] = useState<boolean>(true)
     const episodesPopupTrigger = useRef()
-    const [ episodesListState, updateEpisodesList ] = useState<Record<string,any>>(episodesList)
 
     useEffect(() => {
         if (episodeName && animeTitle) {
@@ -68,20 +67,20 @@ const WatchTopBar = ({ mal, animeId, episodesList, episodeTitle, episodeNumber, 
         <div className="top-bar">
             <div className="top-bar-text">
                 { animeTitle ? <h1 className="top-bar-anime-title">{ animeTitle }</h1> : <div className="anime-title-placeholder loading"></div>}
-                { episodeName ? <motion.p variants={ episodeNameVariants } animate="visible" initial="hidden" className="top-bar-episode-name"><span className="episode-name">{ episodeName }{ getEpisodeTags(episodesListState[episodeNumber - 1]) }</span>{ episodeTitle.length ? <><span style={{ marginTop: 2 }} className="mdi mdi-nm mdi-circle-medium"></span><span title="عنوان الحلقة" dir="ltr" className="episode-title">{ episodeTitle }</span></> : null }</motion.p> : <div className="episode-name-placeholder loading"></div> }
+                { episodeName ? <motion.p variants={ episodeNameVariants } animate="visible" initial="hidden" className="top-bar-episode-name"><span className="episode-name">{ episodeName }{ getEpisodeTags(episodesList[episodeNumber - 1]) }</span>{ episodeTitle.length ? <><span style={{ marginTop: 2 }} className="mdi mdi-nm mdi-circle-medium"></span><span title="عنوان الحلقة" dir="ltr" className="episode-title">{ episodeTitle }</span></> : null }</motion.p> : <div className="episode-name-placeholder loading"></div> }
             </div>
-            { episodesListState.length > 1 ?
+            { episodesList.length > 1 ?
             <motion.div ref={ episodesPopupTrigger } initial={{ scale: 0 }} animate={{ scale: 1.0 }} transition={{ delay: 0.5 }} id="episodes-button" className="floating-button"><span className="mdi mdi-cards-variant"></span>
             الحلقات
             </motion.div> : null }
-            { episodesListState.length > 1 ?
+            { episodesList.length > 1 ?
                 <Popup id="episodes-popup" trigger={ episodesPopupTrigger } title="الحلقات">
                     { ascending ? <>
                         <div onClick={ () => reverseList() } style={{ display: "inline" }} className="dark-button episodes-popup-order">
                             <span className="mdi mdi-sort-ascending"></span>تصاعدي
                         </div>
                         <div id="episodes-list" className="popup-list">
-                            { episodesListState.map((episode,index) => {
+                            { episodesList.map((episode,index) => {
                                 return <Link scroll={ false } href={ "/watch/" + animeId + '-' + mal + "/" + (index + 1) } key={ index }><a onClick={ () => dismissPopup() } className="episode-link">{ `الحلقة ${episode.Episode}${episode.ExtraEpisodes.length ? `-${episode.ExtraEpisodes}` : ""}` }{ getEpisodeTags(episode) }</a></Link>
                             })}
                         </div>
@@ -91,7 +90,7 @@ const WatchTopBar = ({ mal, animeId, episodesList, episodeTitle, episodeNumber, 
                             <span className="mdi mdi-sort-descending"></span>تنازلي
                         </div>
                         <div id="episodes-list" className="popup-list">
-                            { episodesListState.map((episode,index) => {
+                            { episodesList.map((episode,index) => {
                                 return <Link scroll={ false } href={ "/watch/" + animeId + '-' + mal + "/" + (index + 1) } key={ index }><a onClick={ () => dismissPopup() } className="episode-link">{ `الحلقة ${episode.Episode}${episode.ExtraEpisodes.length ? `-${episode.ExtraEpisodes}` : ""}` }{ getEpisodeTags(episode) }</a></Link>
                             }).reverse()}
                         </div>
