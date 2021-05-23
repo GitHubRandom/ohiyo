e4de948ff323c1a8cb0897b5935f838003608486
Mehdi Essalehi <messalehi2001@outlook.com>
2021-05-23 21:17:49 +0100

    Opted out from FLoC

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    async headers() {
+      return [
+        {
+          source: '/(.*?)',
+          headers: [
+            {
+              key: 'Permissions-Policy',
+              value: 'interest-cohort=()',
+            }
+          ],
+        },
+      ]
+    },
+  }
