e15f597f6a8021b4e0ea31a2c36da1438fa74982
Mehdi Essalehi <messalehi2001@outlook.com>
2021-05-25 18:11:17 +0100

    Added watch footer component

---
+++ components/WatchFooter.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+const WatchFooter = () => {
+    return (
+        <footer className="watch-footer">
+            <p className="copyright">كل الحقوق محفوظة لموقع <Link href="/"><a className="copyright-animayhem">Animayhem</a></Link></p>
+        </footer>
+    )
+}
+
+export default WatchFooter
