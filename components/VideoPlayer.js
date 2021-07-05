79e7817bf3653f904ac1a10d0fb7139ab3339c95
Mehdi ESSALEHI <messalehi2001@outlook.com>
2021-07-06 00:35:00 +0100

    Added copy feedback & grouped video overlay

--- components/VideoPlayer.js
+++ components/VideoPlayer.js
@@ -6,15 +6,18 @@ class VideoPlayer extends React.Component {
 
     constructor(props) {
         super(props)
+        this.videoOverlay = React.createRef()
         this.introButton = React.createRef()
         this.videoContainer = React.createRef()
         this.forward = React.createRef()
         this.rewind = React.createRef()
         this.videoAd = React.createRef()
         this.openingHint = React.createRef()
+        this.copyConfirm = React.createRef()
         this.rewindTen = this.rewindTen.bind(this)
         this.forwardTen = this.forwardTen.bind(this)
         this.skipIntro = this.skipIntro.bind(this)
+        this.showCopyConfirm = this.showCopyConfirm.bind(this)
         this.state = {
             showIntro: false
         }
@@ -51,17 +54,7 @@ class VideoPlayer extends React.Component {
         })
         // Why TF did they add crossorigin attribute by default ?w
         document.querySelector(".dplayer-video").removeAttribute("crossorigin")
-        this.videoContainer.current.appendChild(this.introButton.current)
-        if (this.forward.current && this.rewind.current) {
-            this.videoContainer.current.appendChild(this.forward.current)
-            this.videoContainer.current.appendChild(this.rewind.current)
-        }
-        if (this.videoAd.current) {
-            this.videoContainer.current.appendChild(this.videoAd.current)
-        }
-        if (this.openingHint) {
-            this.videoContainer.current.appendChild(this.openingHint.current)
-        }
+        this.videoContainer.current.appendChild(this.videoOverlay.current)
     }
 
     shouldComponentUpdate(nextProps,nextState) {
@@ -97,32 +90,48 @@ class VideoPlayer extends React.Component {
         this.player.destroy()
     }
 
+    showCopyConfirm() {
+        if (this.copyConfirm.current) {
+            this.copyConfirm.current.style.height = "20px"
+            setTimeout(() => {
+                this.copyConfirm.current.style.height = "0"
+            }, 2000)
+        }
+    }
+
     render() {
         return (
             <div dir="ltr" ref={ this.videoContainer } className="anime-video-player">
-                <button onClick={ this.skipIntro } ref={ this.introButton } style={{ display: "none" }} type="button" id="episode-skip-intro">
-                    تخطي المقدمة
-                </button>
-                <div ref={ this.videoAd } id="video-ad">
-                </div>
-                <div style={{ opacity: 0 }} ref={ this.openingHint } className="opening-hint">
-                    <div onClick={ () => this.setState({ showIntro: !this.state.showIntro }) } className="opening-hint-icon">
-                        <span className="mdi mdi-music-note mdi-nm"></span>
+                <div ref={ this.videoOverlay } className="anime-video-overlay">
+                    <button onClick={ this.skipIntro } ref={ this.introButton } style={{ display: "none" }} type="button" id="episode-skip-intro">
+                        تخطي المقدمة
+                    </button>
+                    <div ref={ this.videoAd } id="video-ad">
                     </div>
-                    <div className="opening-hint-text">
-                        <p>
-                            اسم المقدمة
-                        </p>
-                        <p onClick={ () => navigator.clipboard.writeText(this.props.openingName) }>
-                            { this.props.openingName }
-                        </p>
+                    <div style={{ opacity: 0 }} ref={ this.openingHint } className="opening-hint">
+                        <div className="opening-hint-container">
+                            <div onClick={ () => this.setState({ showIntro: !this.state.showIntro }) } className="opening-hint-icon">
+                                <span className="mdi mdi-music-note mdi-nm"></span>
+                            </div>
+                            <div className="opening-hint-text">
+                                <p>
+                                    اسم المقدمة
+                                </p>
+                                <p onClick={ () => { navigator.clipboard.writeText(this.props.openingName); this.showCopyConfirm() } }>
+                                    { this.props.openingName }
+                                </p>
+                            </div>
+                        </div>
+                        <div ref={ this.copyConfirm } className="opening-hint-copy-confirm">
+                            <span className="mdi mdi-content-copy"></span>تم النسخ
+                        </div>
                     </div>
+                    { /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ? <>
+                        <div onClick={ () => document.querySelector(".dplayer-video-current").click() } onDoubleClick={ this.forwardTen } ref={ this.forward } id="forward" className="control-overlay">
+                        </div>  
+                        <div onClick={ () => document.querySelector(".dplayer-video-current").click() } onDoubleClick={ this.rewindTen } ref={ this.rewind } id="rewind" className="control-overlay">
+                        </div></> : null }
                 </div>
-                { /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ? <>
-                    <div onClick={ () => document.querySelector(".dplayer-video-current").click() } onDoubleClick={ this.forwardTen } ref={ this.forward } id="forward" className="control-overlay">
-                    </div>  
-                    <div onClick={ () => document.querySelector(".dplayer-video-current").click() } onDoubleClick={ this.rewindTen } ref={ this.rewind } id="rewind" className="control-overlay">
-                    </div></> : null }
             </div>
         )
     }
