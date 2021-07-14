1fe813b7021c2cc7c96e4d11743bff683ec1c695
Mehdi ESSALEHI <messalehi2001@outlook.com>
2021-07-14 02:16:54 +0100

    Improvements to the video player

--- components/VideoPlayer.js
+++ components/VideoPlayer.js
@@ -4,6 +4,7 @@ const VideoPlayer = ({ introInterval, sources, openingName }) => {
 
     const [ inIntro, setInIntro ] = useState(false)
     const [ showCopyConfirm, setShowCopyConfirm ] = useState(false)
+    const [ dismiss, setDismiss ] = useState(false)
     const player = useRef() // DPlayer
     const videoPlayerContainer = useRef()
     const videoOverlay = useRef()
@@ -30,25 +31,44 @@ const VideoPlayer = ({ introInterval, sources, openingName }) => {
         return () => {
             // Clean effect
             setInIntro(false)
+            setDismiss(false)
             player.current.destroy()
         }
     }, [sources])
 
     useEffect(() => {
-        let introCheck = window.setInterval(() => {
+        if (player.current && introInterval[0] != introInterval[1]) {
+            player.current.options.highlight = [
+                {
+                    text: 'بداية المقدمة',
+                    time: introInterval[0],
+                },
+                {
+                    text: 'نهاية المقدمة',
+                    time: introInterval[1]
+                },
+            ]
+        }
+    }, [introInterval])
+
+    useEffect(() => {
+        let introCheck = !dismiss ? window.setInterval(() => {
             if (player.current) {
                 const currentTime = player.current.video.currentTime
                 if (currentTime >= introInterval[0] && currentTime < introInterval[1]) {
                     setInIntro(true)
-                } else if (inIntro) {
+                } else {
                     setInIntro(false)
                 }
             }
-        }, 1000)
+        }, 1000) : null
         return () => {
-            clearInterval(introCheck)
+            if (introCheck) {
+                clearInterval(introCheck)
+                setInIntro(false)
+            }
         }
-    }, [introInterval, inIntro])
+    }, [introInterval, dismiss])
 
     // An effect for timing out copying opening name confirmation.
     useEffect(() => {
@@ -59,22 +79,29 @@ const VideoPlayer = ({ introInterval, sources, openingName }) => {
         }
     }, [showCopyConfirm])
 
-    const skipIntro = () => {
-        if (player.current && introInterval[0] != introInterval[1]) player.current.seek(introInterval[1])
+    const skipIntro = (event) => {
+        /**
+         * The event is needed to check if skip intro was actually clicked or
+         * the dismiss button.
+         *  */ 
+        if (player.current && introInterval[0] != introInterval[1] && event.target.tagName.toLowerCase() === 'button') player.current.seek(introInterval[1])
     }
 
     const forwardTen = () => {
+        // Forward video 10 secs
         if (player.current) player.current.seek(player.current.video.currentTime + 10)
     }
 
     const rewindTen = () => {
+        // Rewind video 10 secs
         if (player.current) player.current.seek(player.current.video.currentTime - 10)
     }
 
     return (
         <div dir="ltr" ref={ videoPlayerContainer } className="anime-video-player">
             <div ref={ videoOverlay } className="anime-video-overlay">
-                <button onClick={ skipIntro } style={{ display: inIntro ? "block" : "none" }} type="button" id="episode-skip-intro">
+                <button dir="rtl" onClick={ (event) => skipIntro(event) } style={{ display: inIntro ? "block" : "none" }} type="button" id="episode-skip-intro">
+                    <span onClick={ () => setDismiss(true) } className="mdi mdi-close dismiss-skip"></span>
                     تخطي المقدمة
                 </button>
                 <div style={{ opacity: inIntro && openingName.length ? 1 : 0 }} className="opening-hint">
