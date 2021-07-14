e425556cf32ee70084b94f3ca22b583d1dcd103e
Mehdi ESSALEHI <messalehi2001@outlook.com>
2021-07-14 17:28:16 +0100

    Improvements to video player

--- components/VideoPlayer.js
+++ components/VideoPlayer.js
@@ -1,13 +1,40 @@
 import { useRef, useEffect, useState, memo } from 'react'
+import tippy from 'tippy.js'
 
 const VideoPlayer = ({ introInterval, sources, openingName }) => {
 
     const [ inIntro, setInIntro ] = useState(false)
     const [ showCopyConfirm, setShowCopyConfirm ] = useState(false)
     const [ dismiss, setDismiss ] = useState(false)
+    const [ UIShown, setUIShown ] = useState(false)
+    const [ mobileDevice, setMobileDevice ] = useState(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
     const player = useRef() // DPlayer
     const videoPlayerContainer = useRef()
     const videoOverlay = useRef()
+
+    const skipIntro = (event) => {
+        /**
+         * The event is needed to check if skip intro was actually clicked or
+         * the dismiss button.
+         *  */ 
+        console.log(byKeyPress)
+        if (player.current && introInterval[0] != introInterval[1] && event.target.tagName.toLowerCase() === 'button') player.current.seek(introInterval[1])
+    }
+
+    const forwardTen = () => {
+        // Forward video 10 secs
+        if (player.current) player.current.seek(player.current.video.currentTime + 10)
+    }
+
+    const rewindTen = () => {
+        // Rewind video 10 secs
+        if (player.current) player.current.seek(player.current.video.currentTime - 10)
+    }
+
+    useEffect(() => {
+        // Reinit tippy every re-render
+        tippy("[data-tippy-content]")
+    })
 
     // Destroy & initialize player when sources changes
     useEffect(() => {
@@ -27,14 +54,44 @@ const VideoPlayer = ({ introInterval, sources, openingName }) => {
         // DPlayer puts crossorigin attribute by default
         document.querySelector(".dplayer-video").removeAttribute("crossorigin")
         // Reimplement overlay elements (DPlayer removes all container's children).
-        videoPlayerContainer.current.appendChild(videoOverlay.current)    
+        videoPlayerContainer.current.appendChild(videoOverlay.current) 
+        
+        let mutationObserver = new MutationObserver((mutations) => {
+            const classes = mutations[0].target.classList
+            if (classes.contains("dplayer-hide-controller")) {
+                setUIShown(false)
+            } else {
+                setUIShown(true)
+            }
+        })
+
+        mutationObserver.observe(videoPlayerContainer.current, {
+            attributes: true,
+            attributeFilter: ['class'],
+            childList: false,
+            characterData: false
+        })
         return () => {
             // Clean effect
             setInIntro(false)
             setDismiss(false)
+            mutationObserver.disconnect()
             player.current.destroy()
         }
     }, [sources])
+
+    useEffect(() => {
+        const keyPressCallback = event => {
+            let key = event.key
+            if (inIntro && key == "Enter") {
+                player.current.seek(introInterval[1])
+            }
+        }
+        window.addEventListener("keydown", keyPressCallback)
+        return () => {
+            window.removeEventListener("keydown", keyPressCallback)
+        }
+    }, [introInterval, inIntro])
 
     useEffect(() => {
         if (player.current && introInterval[0] != introInterval[1]) {
@@ -79,32 +136,15 @@ const VideoPlayer = ({ introInterval, sources, openingName }) => {
         }
     }, [showCopyConfirm])
 
-    const skipIntro = (event) => {
-        /**
-         * The event is needed to check if skip intro was actually clicked or
-         * the dismiss button.
-         *  */ 
-        if (player.current && introInterval[0] != introInterval[1] && event.target.tagName.toLowerCase() === 'button') player.current.seek(introInterval[1])
-    }
-
-    const forwardTen = () => {
-        // Forward video 10 secs
-        if (player.current) player.current.seek(player.current.video.currentTime + 10)
-    }
-
-    const rewindTen = () => {
-        // Rewind video 10 secs
-        if (player.current) player.current.seek(player.current.video.currentTime - 10)
-    }
-
     return (
         <div dir="ltr" ref={ videoPlayerContainer } className="anime-video-player">
             <div ref={ videoOverlay } className="anime-video-overlay">
-                <button dir="rtl" onClick={ (event) => skipIntro(event) } style={{ display: inIntro ? "block" : "none" }} type="button" id="episode-skip-intro">
-                    <span onClick={ () => setDismiss(true) } className="mdi mdi-close dismiss-skip"></span>
-                    تخطي المقدمة
+                <button dir="rtl" onClick={ (event) => skipIntro(event) } style={{ display: inIntro && UIShown ? "flex" : "none" }} type="button" id="episode-skip-intro">
+                    <span data-tippy-content="إلغاء" onClick={ () => setDismiss(true) } className="mdi mdi-close dismiss-skip"></span>
+                    <span className="skip-intro-text">تخطي المقدمة</span>
+                    { !mobileDevice ? <span className="skip-intro-shortcut">Enter</span> : null}
                 </button>
-                <div style={{ opacity: inIntro && openingName.length ? 1 : 0 }} className="opening-hint">
+                <div style={{ opacity: inIntro && UIShown && openingName.length ? 1 : 0 }} className="opening-hint">
                     <div className="opening-hint-container">
                         <div className="opening-hint-icon">
                             <span className="mdi mdi-music-note mdi-nm"></span>
@@ -121,7 +161,7 @@ const VideoPlayer = ({ introInterval, sources, openingName }) => {
                     <div style={{ height: showCopyConfirm ? "20px" : "0" }} className="opening-hint-copy-confirm">
                         <span className="mdi mdi-content-copy"></span>تم النسخ
                     </div>
-                    { /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ? <>
+                    { mobileDevice ? <>
                         <div onClick={ () => document.querySelector(".dplayer-video-current").click() } onDoubleClick={ forwardTen } id="forward" className="control-overlay">
                         </div>  
                         <div onClick={ () => document.querySelector(".dplayer-video-current").click() } onDoubleClick={ rewindTen }  id="rewind" className="control-overlay">
